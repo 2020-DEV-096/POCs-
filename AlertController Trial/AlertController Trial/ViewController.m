d56be4ab9a061467a678e23d44781d69d724717b
Sameeran Sarangi <sameeran@outlook.com>
2016-04-15 01:08:58 +0530

    Project created

---
+++ AlertController Trial/AlertController Trial/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  AlertController Trial
+//
+//  Created by Sameeran Sarangi on 15/04/16.
+//  Copyright © 2016 Sameeran Sarangi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)clicked:(id)sender {
+    
+}
+
+@end
