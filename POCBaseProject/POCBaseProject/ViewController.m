c5b98e593a996bf7fe90e9cd03c3e782cafc7dbc
Sameeran Sarangi <sameeran@outlook.com>
2016-04-15 23:47:50 +0530

    scrollview and textfield setup

--- POCBaseProject/POCBaseProject/ViewController.m
+++ POCBaseProject/POCBaseProject/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 
 @end
 
@@ -16,12 +18,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UITapGestureRecognizer *tapGest = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapped)];
+    [self.view addGestureRecognizer:tapGest];
+    
+
+}
+-(void)viewDidAppear:(BOOL)animated
+{
+    self.scrollView.contentSize = CGSizeMake(2000, 1000);
+
+    
+}
+-(void)tapped
+{
+    [self.view endEditing:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
