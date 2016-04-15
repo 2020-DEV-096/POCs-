56cf3e6f207ecfa45e7d0659a45f31c68982a854
Sameeran Sarangi <sameeran@outlook.com>
2016-04-15 23:26:45 +0530

    Base project added

---
+++ POCBaseProject/POCBaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  POCBaseProject
+//
+//  Created by Sameeran Sarangi on 15/04/16.
+//  Copyright © 2016 Sameeran Sarangi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
